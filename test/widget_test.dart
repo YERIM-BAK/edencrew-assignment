c246320a41984d966adab9fbd3a4d9377a8274dc
YERIM-BAK <rumiobu03@gmail.com>
2026-09-13 18:00:48 +0900

    fix: 위젯 테스트가 ProviderScope 없이 렌더링되어 실패하던 문제 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:edencrew_assignment_starter/main.dart';
 
 void main() {
   testWidgets('시작 화면이 다크 테마로 렌더링된다', (WidgetTester tester) async {
-    await tester.pumpWidget(const EdencrewAssignmentApp());
+    SharedPreferences.setMockInitialValues({});
+    final prefs = await SharedPreferences.getInstance();
 
-    expect(find.text('이든크루 평가 과제'), findsOneWidget);
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+        child: const EdencrewAssignmentApp(),
+      ),
+    );
+
+    expect(find.text('관심'), findsWidgets);
     expect(
       Theme.of(tester.element(find.byType(Scaffold))).brightness,
       Brightness.dark,
